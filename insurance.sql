7181767ac1a69395564a1ad0998c5379ce38fd7a
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-04 09:35:04 +0530

    Create insurance.sql

---
+++ insurance.sql
@@ -0,0 +1,1 @@
+PERSON (driver id#: string, name: string, address: string) CAR (regno: string, model: string, year: int) ACCIDENT (report_ number: int, acc_date: date, location: string) OWNS (driver id#: string, regno: string) PARTICIPATED(driver id#:string, regno:string, report_ number: int,damage_amount: int)
