e81e4f0939f304324540931afda902661deb0394
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-04 10:05:54 +0530

    Update insurance.sql

--- insurance.sql
+++ insurance.sql
@@ -1,1 +1,36 @@
-PERSON (driver id#: string, name: string, address: string) CAR (regno: string, model: string, year: int) ACCIDENT (report_ number: int, acc_date: date, location: string) OWNS (driver id#: string, regno: string) PARTICIPATED(driver id#:string, regno:string, report_ number: int,damage_amount: int)
+PERSON (driver id#: string, name: string, address: string)
+CAR (regno: string, model: string, year: int) 
+ACCIDENT (report_ number: int, acc_date: date, location: string)
+OWNS (driver id#: string, regno: string)
+PARTICIPATED(driver id#:string, regno:string, report_ number: int,damage_amount: int)
+
+
+
+CREATE TABLE PERSON(
+driver_id char(10) primary key,
+dname varchar(20),
+address text
+);
+
+CREATE TABLE CAR
+(
+reg_no char(10) primary key,
+model varchar(20),
+year int  
+);
+
+CREATE TABLE ACCIDENT
+(
+ report_number int,
+ acc_date date,
+ location text   
+);
+
+CREATE TABLE OWNS
+(
+ driver_id char(10),
+ reg_no char(10), 
+
+
+  
+);
