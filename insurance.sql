75fb40703e1917e1ae219f38074463c62dd1eb53
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-10 20:19:17 +0530

    Update insurance.sql

--- insurance.sql
+++ insurance.sql
@@ -1,37 +1,111 @@
-PERSON (driver id#: string, name: string, address: string)
-CAR (regno: string, model: string, year: int) 
-ACCIDENT (report_ number: int, acc_date: date, location: string)
-OWNS (driver id#: string, regno: string)
-PARTICIPATED(driver id#:string, regno:string, report_ number: int,damage_amount: int)
+DROP DATABASE IF EXISTS insurance;
+CREATE DATABASE insurance;
+USE insurance;
 
-
-
-CREATE TABLE PERSON(
-driver_id char(10) primary key,
-dname varchar(20),
-address text
+CREATE TABLE person (
+    driver_id VARCHAR(255) NOT NULL,
+    driver_name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    PRIMARY KEY (driver_id)
 );
 
-CREATE TABLE CAR
-(
-reg_no char(10) primary key,
-model varchar(20),
-year int  
+CREATE TABLE car (
+    reg_no VARCHAR(255) NOT NULL,
+    model TEXT NOT NULL,
+    c_year INTEGER,
+    PRIMARY KEY (reg_no)
 );
 
-CREATE TABLE ACCIDENT
-(
- report_number int,
- acc_date date,
- location text   
+CREATE TABLE accident (
+    report_no INTEGER NOT NULL,
+    accident_date DATE,
+    location TEXT,
+    PRIMARY KEY (report_no)
 );
 
-CREATE TABLE OWNS
-(
- driver_id char(10),
- reg_no char(10), 
- 
-
-
-  
+CREATE TABLE owns (
+    driver_id VARCHAR(255) NOT NULL,
+    reg_no VARCHAR(255) NOT NULL,
+    FOREIGN KEY (driver_id) REFERENCES person(driver_id) ON DELETE CASCADE,
+    FOREIGN KEY (reg_no) REFERENCES car(reg_no) ON DELETE CASCADE
 );
+
+CREATE TABLE participated (
+    driver_id VARCHAR(255) NOT NULL,
+    reg_no VARCHAR(255) NOT NULL,
+    report_no INTEGER NOT NULL,
+    damage_amount FLOAT NOT NULL,
+    FOREIGN KEY (driver_id) REFERENCES person(driver_id) ON DELETE CASCADE,
+    FOREIGN KEY (reg_no) REFERENCES car(reg_no) ON DELETE CASCADE,
+    FOREIGN KEY (report_no) REFERENCES accident(report_no)
+);
+
+INSERT INTO person VALUES
+("D111", "Driver_1", "Kuvempunagar, Mysuru"),
+("D222", "Smith", "JP Nagar, Mysuru"),
+("D333", "Driver_3", "Udaygiri, Mysuru"),
+("D444", "Driver_4", "Rajivnagar, Mysuru"),
+("D555", "Driver_5", "Vijayanagar, Mysore");
+
+INSERT INTO car VALUES
+("KA-20-AB-4223", "Swift", 2020),
+("KA-20-BC-5674", "Mazda", 2017),
+("KA-21-AC-5473", "Alto", 2015),
+("KA-21-BD-4728", "Triber", 2019),
+("KA-09-MA-1234", "Tiago", 2018);
+
+INSERT INTO accident VALUES
+(43627, "2020-04-05", "Nazarbad, Mysuru"),
+(56345, "2019-12-16", "Gokulam, Mysuru"),
+(63744, "2020-05-14", "Vijaynagar, Mysuru"),
+(54634, "2019-08-30", "Kuvempunagar, Mysuru"),
+(65738, "2021-01-21", "JSS Layout, Mysuru"),
+(66666, "2021-01-21", "JSS Layout, Mysuru");
+
+INSERT INTO owns VALUES
+("D111", "KA-20-AB-4223"),
+("D222", "KA-20-BC-5674"),
+("D333", "KA-21-AC-5473"),
+("D444", "KA-21-BD-4728"),
+("D222", "KA-09-MA-1234");
+
+INSERT INTO participated VALUES
+("D111", "KA-20-AB-4223", 43627, 20000),
+("D222", "KA-20-BC-5674", 56345, 49500),
+("D333", "KA-21-AC-5473", 63744, 15000),
+("D444", "KA-21-BD-4728", 54634, 5000),
+("D222", "KA-09-MA-1234", 65738, 25000);
+
+SELECT COUNT(driver_id)
+FROM participated p, accident a
+WHERE p.report_no = a.report_no AND a.accident_date LIKE "2021%";
+
+SELECT COUNT(DISTINCT a.report_no)
+FROM accident a
+WHERE EXISTS
+    (SELECT *
+     FROM person p, participated ptd
+     WHERE p.driver_id = ptd.driver_id AND p.driver_name = "Smith" AND a.report_no = ptd.report_no);
+
+INSERT INTO accident VALUES
+(45562, "2024-04-05", "Mandya");
+
+INSERT INTO participated VALUES
+("D222", "KA-21-BD-4728", 45562, 50000);
+
+DELETE FROM car
+WHERE model = "Mazda" AND reg_no IN
+    (SELECT car.reg_no
+     FROM person p, owns o
+     WHERE p.driver_id = o.driver_id AND o.reg_no = car.reg_no AND p.driver_name = "Smith");
+
+UPDATE participated SET damage_amount=10000 WHERE report_no=65738 AND reg_no="KA-09-MA-1234";
+
+CREATE VIEW CarsInAccident AS
+SELECT DISTINCT model, c_year
+FROM car c, participated p
+WHERE c.reg_no = p.reg_no;
+
+SELECT * FROM CarsInAccident;
+
+CREATE VIEW DriversWithCar
